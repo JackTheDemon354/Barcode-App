989c326228b6394f75bd7ff3e6fef2ee5df71af0
PVP-Arena <jack.vanwijk@hotmail.com>
2025-07-16 17:41:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Serve static files from 'public' folder (your index.html and others)
+// Serve static files from 'public' folder
 app.use(express.static('public'));
 
 const DATA_FILE = path.join(__dirname, 'data.json');
 
-// Load data from file or start empty
+// Load data or start empty
 let products = {};
 try {
   const data = fs.readFileSync(DATA_FILE);
@@ -21,12 +21,12 @@ try {
   products = {};
 }
 
-// Save data to file
+// Save products
 function saveData() {
   fs.writeFileSync(DATA_FILE, JSON.stringify(products, null, 2));
 }
 
-// API: Get product by barcode
+// Get product by barcode
 app.get('/product/:barcode', (req, res) => {
   const barcode = req.params.barcode;
   if (products[barcode]) {
@@ -36,7 +36,7 @@ app.get('/product/:barcode', (req, res) => {
   }
 });
 
-// API: Add or update product
+// Add or update product
 app.post('/product', (req, res) => {
   const { barcode, name, price } = req.body;
   if (!barcode || !name || !price) {
@@ -45,6 +45,18 @@ app.post('/product', (req, res) => {
   products[barcode] = { barcode, name, price };
   saveData();
   res.json(products[barcode]);
+});
+
+// Delete product
+app.delete('/product/:barcode', (req, res) => {
+  const barcode = req.params.barcode;
+  if (products[barcode]) {
+    delete products[barcode];
+    saveData();
+    res.json({ message: 'Deleted' });
+  } else {
+    res.status(404).json({ error: 'Not found' });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
